6ec2d87776deb7af49a5742ede4362db7904f78e
Utku <utkuarslan07@gmail.com>
2022-12-02 21:38:59 +0300

    Add files via upload

---
+++ ProfitCalculation.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestProfit
+{
+    public class ProfitCalculation
+    {
+        
+        public int[] valueInput { get { return valueArray; } set { valueArray = value; } }
+        private int[] valueArray;
+        public int startIndex { get { return _startIndex; } }
+        private int _startIndex = 0;
+        private int _currentIndex = 0;
+
+        public int findNextSale()
+        {
+            int index = startIndex;
+            bool increasing = false;
+           for(int i = startIndex+1; i < valueInput.Length; i++)
+            {
+                if (valueInput[index] < valueInput[i])
+                {
+                    index = i;
+                    increasing = true;
+                }
+                else if (increasing && valueInput[index] > valueInput[i]) return (_currentIndex=index);
+            }
+            return -1;
+        }
+
+        #region StartIndex
+        /// <summary>
+        /// "Örnek senaryoda bir yatırımcı ucuz olan başlangıca en yakın günden..."
+        /// </summary>
+        /// <returns>En uygun başlangıç değerinin 'array' içindeki konumunu döndürür</returns>
+        public int getOptimumStartIndex()
+        {
+            return getOptimumBuyIndex(0);
+        }
+        public int getOptimumBuyIndex()
+        {
+            return getOptimumBuyIndex(_currentIndex);
+        }
+         public int getOptimumBuyIndex(int _start)
+        {
+            if (valueArray == null || _start + 1 >= valueArray.Length) return -1;
+
+            int index= _start+1;
+            float score= scoreBuyIndex(index);
+            for(int s = _start+1; s < valueArray.Length; s++)
+            {
+                float tempScore = scoreBuyIndex(s);
+                if (score < tempScore)
+                {
+                    index = s;
+                    score = tempScore;
+                }
+            }
+            _startIndex = index;
+            _currentIndex = index;
+            return index;
+        }
+
+        public float scoreBuyIndex(int startIndex)
+        {
+            float sales = countPosibleSalesOfStartIndex(startIndex) * 2f;
+            return (sales + (valueInput.Length - startIndex)) / 3;
+        }
+        public int countPosibleSalesOfStartIndex(int startIndex)
+        {
+            int value = valueInput[startIndex];
+            int count = 0;
+            for(int i = startIndex; i < valueInput.Length; i++)
+            {
+                if (value < valueInput[i]) count++;
+            }
+            return count;
+        }
+
+        #endregion
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestProfit
+{
+    class Program
+    {
+
+        public static int[] example_1 = { 100, 50, 200, 400, 20, 60, 10, 90, 300, 200};
+        public static int[] example_2 = { 20, 30, 40, 10, 5, 80, 100, 60};
+        public static int[] example_3 = { 20, 10, 5, 30, 60, 90, 40, 50 };
+        public static int[] example_4 = { 20, 5, 15, 35, 10, 50, 80, 40 };
+
+        static void Main(string[] args)
+        {
+            //selecting the example
+            int[] arr = example_4;
+
+            //displayinh the array
+            Console.Write("[");
+            for(int i = 0; i<arr.Length; i++) 
+                Console.Write((i==0?"":",") + arr[i]);
+            Console.WriteLine("]");
+            Console.WriteLine("");
+
+            //initializing the class
+            ProfitCalculation pr = new ProfitCalculation();
+            pr.valueInput = arr;
+            
+            //Declaring integers
+            int buy = 0,sell=0,profit=-1,
+                boughtCount=0,total=0;
+
+            while (true)
+            {
+                buy = pr.getOptimumBuyIndex();  // find where to buy as a index in array
+                if (buy == -1) break;           // check, is bought or not. Else "buy" value is "-1"
+                sell = pr.findNextSale();       // find sale index
+                if (sell == -1) break;          // check, is sold or not. Else "sell" value is "-1"
+                if (profit == -1) profit = pr.valueInput[buy];  //first amount is given in here to purchase single share to codes, in first loop
+
+                boughtCount = profit / pr.valueInput[buy];                          //calculates, how much shares are bought
+                profit = (pr.valueInput[sell] - pr.valueInput[buy]) *boughtCount;   //calculates profit
+                total = pr.valueInput[sell] * boughtCount;                          // calculates total amount
+
+                Console.WriteLine("[BUY] day:"+(buy+1)+" - at:" + pr.valueInput[buy] + " - "+boughtCount+"X times 	/ [SELL] day:" + (sell+1)+ " - at:"+ pr.valueInput[sell]+ " 	/ profit: " + (profit ));
+
+            }
+            Console.WriteLine("Total: "+ total);
+
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            
+        }
+
+
+
+    }
+}
